be041269b49ba71a846b13c6e9b4b2ed0b403e2a
Chris Murphy <chris@strandz.org>
2016-12-11 14:00:14 +1030

    instead bots lets use entities

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 
-8 Done with simple hardware instructions, with no need for any matrix operations, and same code used whether rotating a column or a row  
+Uses Instaparse for parsing, unless needs to be dynamic, in which case simple reduction used (for instance day 9).
+
+Day 8 is done with simple hardware instructions, with no need for any matrix operations, which means essentially the
+same code can be used whether rotating a column or a row.
--- advent/ten.clj
+++ advent/ten.clj
@@ -1,5 +1,6 @@
 (ns advent.ten
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [utils :as u]))
 
 (def test-input-line "bot 75 gives low to bot 145 and high to bot 95")
 (def test-lines ["value 5 goes to bot 2"
@@ -26,9 +27,80 @@
          {:int clojure.edn/read-string}))
   )
 
+(defn produce-instructions [hiccup]
+  (reduce
+    (fn [acc [cmd & tail]]
+      (case cmd
+        :value-goes (let [[value bot-num] tail]
+                      (conj acc {:bot bot-num :value value}))
+        :bot-gives acc))
+    []
+    hiccup))
+
+(defn produce-bots-hash [hiccup]
+  (reduce
+    (fn [acc [cmd & tail]]
+      (case cmd
+        :value-goes acc
+        :bot-gives (let [[bot-num [low-entity-type] low-entity-value [high-entity-type] high-entity-value] tail]
+                     (assoc acc bot-num {:low {:type low-entity-type :entity-num low-entity-value}
+                                         :high {:type high-entity-type :entity-num high-entity-value}
+                                         :values []}))))
+    {}
+    hiccup))
+
+;;
+;; Returns a bots state after an instruction.
+;;
+(defn give-one [bots]
+  ;(println bots)
+  (fn [{:keys [from-bot bot value] :as instruction}]
+    (let [_ (println "getting " instruction)
+          retrieved-bot (bots bot)
+          new-bot (update retrieved-bot :values conj value)
+          _ (when from-bot
+              (assert (number? from-bot) (str "Not number: " (type from-bot)))
+              (println (str "to rem: " value " from " from-bot))
+              ;(println (str "from bot values before: " (:values (get bots from-bot))))
+              )
+          new-state (-> bots
+                        (assoc bot new-bot)
+                        (update-in [from-bot :values] (fn [old-values]
+                                                        ;(println "b4:" old-values)
+                                                        (let [res (vec (remove #{value} old-values))
+                                                              ;_ (println "after:" res)
+                                                              ]
+                                                          res))))
+          ;_ (when from-bot (println "from bot values after: " (:values (get new-state from-bot))))
+          ]
+      (if (> (-> new-bot :values count) 1)
+        (let [bot-values (:values new-bot)
+              _ (assert (= 2 (count bot-values)) (str "More than 2: " new-bot))
+              lower (apply min bot-values)
+              higher (apply max bot-values)
+              _ (assert (not= lower higher))
+              low-receiver (:low retrieved-bot)
+              _ (println (str "low " lower " goes to" low-receiver))
+              high-receiver (:high retrieved-bot)
+              _ (println (str "high " higher " goes to" high-receiver))
+              changed-state-1 ((give-one new-state) {:from-bot bot :bot (:entity-num low-receiver) :value lower})
+              changed-state-2 ((give-one changed-state-1) {:from-bot bot :bot (:entity-num high-receiver) :value higher})
+              ]
+          changed-state-2)
+        (let []
+          new-state)))))
+
 (defn x []
-  (->> test-lines
-       (map to-hiccup)))
+  (let [hiccup (map to-hiccup test-lines)
+        instructions (produce-instructions hiccup)
+        bots (produce-bots-hash hiccup)
+        new-bots (reduce
+                   (fn [bots instruction]
+                     ((give-one bots) instruction))
+                   bots
+                   instructions)
+        ]
+    new-bots))
 
 (defn x-1 []
   (to-hiccup (second test-lines)))
