3f3368c7ddb9748c5233462319f725f762555ca9
Chris Murphy <chris@strandz.org>
2016-12-13 04:00:48 +1030

    Getting there

--- dev/server/advent/eleven.clj
+++ dev/server/advent/eleven.clj
@@ -1,5 +1,6 @@
 (ns advent.eleven
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [clojure.pprint :as pp]))
 
 (def example-in ["The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip."
                  "The second floor contains a hydrogen generator."
@@ -20,7 +21,7 @@
      generator = word <' generator'>
      microchip = word <'-compatible microchip'>
      <item> = generator | microchip
-     <items> = (<' a '> item <','> ?) + <' and a '> item
+     <items> = (<' a '> item <','> ?) + ( <' and a '> item ) ?
      <contents> = (items | <' nothing relevant'>) <'.'>
      sentence = <'The '> floor <' floor contains'> contents"))
 
@@ -29,12 +30,104 @@
        (insta/transform
          {:floor (fn [x]
                    (case x
-                     "first" [:floor-num 1]
-                     "second" [:floor-num 2]
-                     "third" [:floor-num 3]
-                     "fourth" [:floor-num 4]))})))
+                     "first" :F1
+                     "second" :F2
+                     "third" :F3
+                     "fourth" :F4))})))
+
+(def example-floor [[:generator "thulium"] [:microchip "thulium"] [:generator "plutonium"] [:generator "strontium"]])
+(def example-chip-fried-floor [[:microchip "thulium"] [:generator "plutonium"] [:generator "strontium"]])
+(def example-safe-floor [[:generator "plutonium"] [:generator "strontium"]])
+
+(def example-lab {:elevator [:F1 [[:generator "plutonium"] [:generator "strontium"]]]
+                  :F1 []
+                  :F2 []
+                  :F3 []
+                  :F4 []})
+
+(defn parse-floors [in]
+  (let [f1 #(vec (next (to-hiccup %)))
+        f2 #(vector (first %) (vec (next %)))]
+    (into {} (mapv (comp f2 f1) in))))
 
 (defn x-1 []
-  (vec (next (to-hiccup (nth input 0)))))
+  (parse-floors input))
 
+(defn safe-floor? [floor]
+  (let [chips (map second (filter #(= (first %) :microchip) floor))
+        generators (into #{} (map second (filter #(= (first %) :generator) floor)))]
+    (every? generators chips)))
 
+;;
+;; Each possible load has one or two items in it. eg [[:generator "plutonium"] [:generator "strontium"]]
+;; or [[:generator "strontium"]] are both lift loads
+;;
+(defn load-lift-combinations [floor]
+  (let [single-fills (set (map (comp set vector) floor))]
+    (into single-fills (for [i floor
+                             j floor
+                             :when (and (not= i j) (safe-floor? [i j]))]
+                         (into #{} [i j])))))
+
+(defn x-2 []
+  (assert (= [true false true] (map safe-floor? [example-floor example-chip-fried-floor example-safe-floor]))))
+
+;;
+;; State does not need to include any contents of elevator, because they are the transitions
+;;
+(def starting-lab {:elevator :F3
+                   :F1 [[:generator "thulium"] [:microchip "thulium"] [:generator "plutonium"] [:generator "strontium"]],
+                   :F2 [[:microchip "plutonium"] [:microchip "strontium"]],
+                   :F3 [[:generator "promethium"] [:microchip "promethium"] [:generator "ruthenium"] [:microchip "ruthenium"]],
+                   :F4 []})
+
+(defn x-3 []
+  (load-lift-combinations example-floor)
+  #_(advent.day11/select-elevator-items example-floor))
+
+;;
+;; State transition based on a generated lift load.
+;; We need to take from current and put into new floor, and move the lift
+;;
+(defn move-one [lab to-floor-id lift-load]
+  (let [_ (assert (set? lift-load))
+        current-floor-id (:elevator lab)
+        _ (assert current-floor-id (str "Strange lab no elevator"))
+        ]
+    (-> lab
+        (assoc :elevator to-floor-id)
+        (update current-floor-id (fn [floor-content]
+                                   ;(println (str "To rm " lift-load " from " floor-content))
+                                   (vec (remove lift-load floor-content))))
+        (update to-floor-id into lift-load))))
+
+(defn x-4 []
+  (let [res (move-one starting-lab :F4 #{[:generator "ruthenium"] [:microchip "ruthenium"]})]
+    (println "F4" (:F4 res) "F3" (:F3 res) "elevator:" (:elevator res))))
+
+(def next-floors {:F4 '(:F3)
+                  :F3 '(:F4 :F2)
+                  :F2 '(:F1 :F3)
+                  :F1 '(:F2)})
+
+;;
+;; Start off with one lab and produce tonnes.
+;; There are floors above and below.
+;; There are all the possible take outs from the current floor.
+;; All possible new labs come from calling move-one.
+;; Then remove all that don't cause problems.
+;;
+(defn generate-possible-moves [lab-in]
+  (let [current-floor (:elevator lab-in)
+        ]
+    (for [new-floor (next-floors current-floor)
+          lift-combo (load-lift-combinations (current-floor lab-in))
+          :let [lab-out (move-one lab-in new-floor lift-combo)]
+          :when (and (safe-floor? (current-floor lab-out))
+                     (safe-floor? (new-floor lab-out)))]
+      lab-out)))
+
+(defn x-5 []
+  (let [res (generate-possible-moves starting-lab)]
+    (pp/pprint res)
+    (println (count res))))
