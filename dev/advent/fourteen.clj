a33f0a74e6f908676b27c9e0b0ab777cd6527142
Chris Murphy <chris@strandz.org>
2016-12-15 04:34:52 +1030

    On much better path now for 14 - yet to overlay concept of order so works as if only one running at a time

--- dev/advent/fourteen.clj
+++ dev/advent/fourteen.clj
@@ -1,18 +1,24 @@
 (ns advent.fourteen
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pp]))
 
 (import 'java.security.MessageDigest
         'java.math.BigInteger)
 
+(defn decent-hash? [hash]
+  hash)
+
 (defn five-times? [sub-seq]
+  (assert (= 5 (count sub-seq)) (str "Not length 5: <" sub-seq ">"))
+  (assert (= 1 (count (frequencies sub-seq))))
   (fn [hash]
-    (let [res (str/index-of hash sub-seq)]
-      res)))
+    (assert (decent-hash? hash))
+    (str/index-of hash sub-seq)))
 
 (defn triple [hash]
   (first (drop-while #(< (count %) 3) (partition-by identity hash))))
 
-(defn md5 [s]
+(defn- -md5 [s]
   (let [algorithm (MessageDigest/getInstance "MD5")
         size (* 2 (.getDigestLength algorithm))
         raw (.digest algorithm (.getBytes s))
@@ -20,12 +26,70 @@
         padding (apply str (repeat (- size (count sig)) "0"))]
     (str padding sig)))
 
-(defn search [seed]
+(def md5 (memoize -md5))
+
+(defn update-triples [maybe-triple idx triples]
+  (let [
+        ;_ (println "at" idx (count triples))
+        old-removed (remove (fn [{:keys [index]}]
+                              (let [age (- idx index)]
+                                (>= age 1000))) triples)
+        new-added (if maybe-triple
+                    (conj old-removed maybe-triple)
+                    old-removed)]
+    new-added))
+
+(defn triple-remover [five-timers-orig-indexes]
+  (assert (set? five-timers-orig-indexes))
+  (fn [{:keys [index]}]
+    (five-timers-orig-indexes index)))
+
+(defn search-2 [seed]
+  (loop [idx 0
+         result-keys []
+         triples []]
+    (let [hash-val (md5 (str seed idx))
+          _ (assert (decent-hash? hash-val) (str "Did not manage to get a hash: " seed ", " idx))
+          triple? (triple hash-val)]
+      (if (> (count result-keys) 64)
+        result-keys
+        (let [new-triple (when triple?
+                           {:triple triple?
+                            :index  idx
+                            :hash   hash-val
+                            :five-times-fn (five-times? (apply str (repeat 5 (first triple?))))})
+              updated-triples (update-triples new-triple idx triples)
+              ;_ (println updated-triples)
+              found-five-timers (reduce
+                                  (fn [acc {:keys [five-times-fn index hash]}]
+                                    (let [res? (five-times-fn hash-val)]
+                                      (if res?
+                                        (let [new-acc (conj acc {:orig-hash hash
+                                                                 :five-times-hash hash-val
+                                                                 :orig-index index
+                                                                 :found-index idx})
+                                              ;_ (println "new-acc: " new-acc)
+                                              ]
+                                          new-acc)
+                                        acc)))
+                                  []
+                                  updated-triples)
+              ;_ (println found-five-timers)
+              remover (triple-remover (set (map :orig-index found-five-timers)))
+              re-updated-triples (remove remover updated-triples)
+              ;_ (println re-updated-triples)
+              new-result-keys (concat result-keys found-five-timers)
+              ]
+          (recur (inc idx) new-result-keys re-updated-triples)
+          )))))
+
+(defn search-1 [seed]
   (loop [mode :triple
          idx 0
-         thousand {}
+         thousand {:max-triple-idx 0}
          result-keys []]
     (let [hash-val (md5 (str seed idx))
+          _ (assert (decent-hash? hash-val) (str "Did not manage to get a hash: " seed ", " idx))
           trip? (triple hash-val)]
       (if (= 64 (count result-keys))
         result-keys
@@ -35,6 +99,7 @@
                            (inc idx)
                            {:resume-at-idx   (inc idx)
                             :countdown       999
+                            :max-triple-idx  (:max-triple-idx thousand)
                             :revisit-triples (or (:revisit-triples thousand) [])
                             :sub-seq-fn      (five-times? (apply str (repeat 5 (first trip?))))}
                            result-keys)
@@ -48,13 +113,19 @@
                         ;; one to jump to, and keep doing this
                         ;;
                         (let [
-                              [head second & tail] revisit-triples
-                              _ (println (str "Instead staying at " idx " want to move s/how to " (vec revisit-triples)))
-                              _ (assert resume-at-idx (str "No resume recorded in thousand: " (dissoc thousand :revisit-triples) ", so far: " result-keys))
+                              ;[head second & tail] revisit-triples
+                              [[index trip?] & tail] (drop-while (fn [[i _ _]] (< i idx)) revisit-triples)
+                              _ (println (str "Instead staying at " idx " want to move s/how to " index ", then: " trip?))
+                              ;_ (assert resume-at-idx (str "No resume recorded in thousand: " (dissoc thousand :revisit-triples) ", so far: " result-keys))
                               ]
-                          (recur mode resume-at-idx (assoc thousand :revisit-triples tail) result-keys))
+                          (recur :five-in-thousand index (assoc thousand :revisit-triples tail
+                                                                         :resume-at-idx   (inc index)
+                                                                         :countdown       999
+                                                                         :sub-seq-fn      (five-times? (apply str (repeat 5 (first trip?))))) result-keys))
                         (recur mode (inc idx) thousand result-keys))))
-          :five-in-thousand (let [{:keys [countdown revisit-triples sub-seq-fn resume-at-idx]} thousand
+          :five-in-thousand (let [{:keys [countdown revisit-triples sub-seq-fn resume-at-idx max-triple-idx]} thousand
+                                  _ (assert max-triple-idx)
+                                  _ (assert sub-seq-fn)
                                   has-five? (sub-seq-fn hash-val)
                                   ]
                               (if (zero? countdown)
@@ -62,18 +133,23 @@
                                 (let [-resume-idx (if (seq revisit-triples)
                                                    (ffirst revisit-triples)
                                                    resume-at-idx)]
-                                  (recur :triple -resume-idx {:revisit-triples revisit-triples} result-keys))
+                                  (recur :triple -resume-idx {:revisit-triples revisit-triples :max-triple-idx max-triple-idx} result-keys))
                                 (if has-five?
-                                  (let [result-key [(dec resume-at-idx) idx hash-val countdown]]
-                                    (recur :triple resume-at-idx {:revisit-triples revisit-triples} (conj result-keys result-key)))
-                                  (let [new-revisit-triples (if trip?
-                                                              (conj revisit-triples [idx trip? hash-val])
+                                  (let [_ (assert (decent-hash? hash-val))
+                                        result-key [(dec resume-at-idx) idx hash-val countdown]]
+                                    (println "Banking: " result-key)
+                                    (recur :triple resume-at-idx {:revisit-triples revisit-triples :max-triple-idx max-triple-idx} (conj result-keys result-key)))
+                                  (let [new-max-triple-idx? (and trip? (> idx max-triple-idx))
+                                        maybe-new-max-triple-idx-value (if new-max-triple-idx? idx max-triple-idx)
+                                        new-revisit-triples (if new-max-triple-idx?
+                                                              (conj revisit-triples [idx trip?])
                                                               revisit-triples)]
                                     (recur mode
                                            (inc idx)
                                            (-> thousand
                                                (update :countdown dec)
-                                               (assoc :revisit-triples new-revisit-triples))
+                                               (assoc :revisit-triples new-revisit-triples)
+                                               (assoc :max-triple-idx maybe-new-max-triple-idx-value))
                                            result-keys))))))))))
 
 (def seed "abc")
@@ -88,4 +164,4 @@
     ((five-times? to-search) five-times-sample)))
 
 (defn x []
-  (search seed))
+  (pp/pprint (search-2 seed)))
