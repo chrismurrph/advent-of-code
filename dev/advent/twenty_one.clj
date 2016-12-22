72de293734f7febc7d607e02153890e4e471a8de
Chris Murphy <chris@strandz.org>
2016-12-23 06:13:39 +1030

    21 first part, but only with some help

--- dev/advent/twenty_one.clj
+++ dev/advent/twenty_one.clj
@@ -1,6 +1,8 @@
 (ns advent.twenty-one
   (:require [instaparse.core :as insta]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.pprint :as pp]
+            [utils :as u])
   (:import (java.io StringReader BufferedReader)))
 
 (def steps ["move position 0 to position 3"
@@ -10,6 +12,7 @@
             "swap letter h with letter b"
             "swap position 6 with position 3"
             "reverse positions 1 through 3"
+            "rotate based on position of letter c"
             ])
 
 (def grammar-1
@@ -17,78 +20,127 @@
     "<S> = cmd
      move = <'move position '> int <' to position '> int
      direction = 'right' | 'left'
-     rotate = <'rotate '> direction <' '> int (<' steps'> | <' step'>)
+     rotate-direction = <'rotate '> direction <' '> int (<' steps'> | <' step'>)
+     rotate-based = <'rotate based on position of letter '> letter
      letter = #'\\w+'
      swap-letter = <'swap letter '> letter <' with letter '> letter
      swap-position = <'swap position '> int <' with position '> int
      reverse = <'reverse positions '> int <' through '> int
-     cmd = move | rotate | swap-letter | swap-position | reverse
+     cmd = move | rotate-direction | rotate-based | swap-letter | swap-position | reverse
      int = #'[-0-9]+'
      "
     ))
 
-(defn swap-position [x y]
-  (fn [s]
-    (let [letter-at-x (nth s x)
-          letter-at-y (nth s y)
-          before (subs s 0 x)
-          middle (subs s (inc x) y)
-          after (subs s (inc y) (count s))
-          res (str before letter-at-y middle letter-at-x after)]
-      res)))
+(defn left-rotate [s n]
+  (let [rem-n (rem n (count s))]
+    (str (subs s rem-n) (subs s 0 rem-n))))
 
-(defn left-rotate [n]
-  (fn [s]
-    (str (subs s n) (subs s 0 n))))
+(defn right-rotate [s n]
+  (let [size (count s)
+        rem-n (rem n size)
+        o (- size rem-n)
+        len size]
+    (str (subs s o len) (subs s 0 o))))
 
-(defn right-rotate [n]
-  (fn [s]
-    (let [o (- (count s) n)
-          len (count s)]
-      (str (subs s o len) (subs s 0 o)))))
+;; rotate based on position of letter d finds the index of letter d (4), then rotates the string right once, plus a number of times
+;; equal to that index, plus an additional time because the index was at least 4, for a total of 6 right rotations: decab.
+(defn rotate-position [s a]
+  (let [x (str/index-of s a)
+        extra (if (>= x 4) 1 0)
+        times (+ 1 x extra)
+        ;_ (println times s)
+        ]
+    (right-rotate s times)))
 
-(defn rotate-position [a]
-  (fn [s]
-    (let [x (str/index-of s a)
-          extra (if (>= x 4) 1 0)
-          times (+ 1 x extra)]
-      ((right-rotate times) s))))
-
-(defn reverse-span [m n]
-  (fn [s]
-    (let [sub (subs s m (inc n))
-          before (subs s 0 m)
-          after (subs s (inc n) (count s))
-          rev (str/reverse sub)]
-      ;[m n s before rev after]
-      (str before rev after)
-      )))
+(defn reverse-span [s m n]
+  (let [sub (subs s m (inc n))
+        before (subs s 0 m)
+        after (subs s (inc n) (count s))
+        rev (str/reverse sub)]
+    ;[m n s before rev after]
+    (str before rev after)
+    ))
 
 (defn insert [s idx letter]
   (let [before (subs s 0 idx)
         after (subs s idx (count s))]
     (str before letter after)))
 
-(defn move [m n]
-  (fn [s]
-    (let [to-move (get s m)
-          before (subs s 0 m)
-          after (subs s (inc m) (count s))
-          now-removed (str before after)
-          res (insert now-removed n to-move)]
-      ;[now-removed to-move n res]
-      res)))
+(defn move [s m n]
+  (let [to-move (get s m)
+        before (subs s 0 m)
+        after (subs s (inc m) (count s))
+        now-removed (str before after)
+        res (insert now-removed n to-move)]
+    ;[now-removed to-move n res]
+    res))
 
-(defn swap-letter-hof [a b]
-  (fn [s]
-    (let [
-          x (str/index-of s a)
-          y (str/index-of s b)
-          before (subs s 0 x)
-          middle (subs s (inc x) y)
-          after (subs s (inc y) (count s))
-          res (str before b middle a after)]
-      res)))
+(defn -swap-letter-wrong [s b a]
+  (let [
+        -x (str/index-of s a)
+        -y (str/index-of s b)
+        x-use (min -x -y)
+        y-use (max -x -y)
+        _ (assert (> y-use x-use))
+        ;_ (println "swapping letter" a "at" x-use)
+        ;_ (println "with letter" b "at" y-use "in" s)
+        before (subs s 0 x-use)
+        _ (assert (not (neg? x-use)))
+        _ (assert (not (neg? y-use)))
+        _ (assert (string? s))
+        middle (subs s (inc x-use) y-use)
+        after (subs s (inc y-use) (count s))
+        res (str before b middle a after)]
+    res))
+
+(defn fixed-swap-letter [s y x]
+  (let [use-x (first x)
+        use-y (first y)]
+    (apply str (replace {use-x use-y use-y use-x} s))))
+
+(defn swap-position [s -y -x]
+  (let [
+        x-use (min -x -y)
+        y-use (max -x -y)
+        letter-at-x (nth s x-use)
+        letter-at-y (nth s y-use)
+        before (subs s 0 x-use)
+        middle (subs s (inc x-use) y-use)
+        after (subs s (inc y-use) (count s))
+        res (str before letter-at-y middle letter-at-x after)]
+    res))
+
+;; Bruce's operations
+(defn bruce-swap-letter [s x y]   (replace {x y y x} s))
+
+(defn bruce-swap-position [s x y] (bruce-swap-letter s (nth s x) (nth s y)))
+
+(defn bruce-rotate-left [s n]     (take (count s) (drop n (cycle s))))
+
+(defn bruce-rotate-right [s n]    (reverse (bruce-rotate-left (reverse s) n)))
+
+(defn bruce-move-position [s x y]
+  (let [l   (nth s x)
+        res (remove #(= l %) s)]
+    (concat (take y res) (cons l (drop y res)))))
+
+(defn bruce-reverse-positions [s x y]
+  (let [part-size (inc (- y x))]
+    (concat (take x s)
+            (reverse (take part-size (drop x s)))
+            (drop (+ x part-size) s))))
+
+(defn bruce-rotate-based [s l]
+  (let [i (.indexOf s l)]
+    (bruce-rotate-right s (+ (inc i) (if (>= i 4) 1 0)))))
+
+(def rotate-based #_rotate-position bruce-rotate-based #_(fn [_ _] (assert false)))
+(def reverse-positions #_reverse-span bruce-reverse-positions)
+(def move-position #_move bruce-move-position)
+(def rotate-right #_right-rotate bruce-rotate-right #_(fn [_ _] (assert false)))
+(def rotate-left #_left-rotate bruce-rotate-left)
+(def swap-position #_swap-position bruce-swap-position #_(fn [_ _ _] (assert false)))
+(def swap-letter fixed-swap-letter #_bruce-swap-letter #_(fn [_ _ _] (assert false)))
 
 (defn to-hiccup [s]
   (->> (first (grammar-1 s))
@@ -107,19 +159,74 @@
         ]
     (mapcat identity (map next instructions))))
 
+;; Mine are string based, so needed this when starting to use Bruce's functions
+(def trans (partial apply (comp u/probe-on str)))
+
+(defn execute-cmd [cmd acc arg-1 arg-2]
+  (case cmd
+    :move (move-position acc arg-1 arg-2)
+    :rotate-based (rotate-based acc arg-1)
+    :rotate-direction (cond
+                        (= :right arg-1)
+                        (rotate-right acc arg-2)
+                        (= :left arg-1)
+                        (rotate-left acc arg-2)
+                        )
+    :swap-letter (swap-letter acc arg-1 arg-2)
+    :swap-position (swap-position acc arg-1 arg-2)
+    :reverse (reverse-positions acc arg-1 arg-2)))
+
+(defn run-instructions [instructions]
+  (let [res (reduce
+              (fn [acc ele]
+                (let [
+                      ;_ (println ele)
+                      [cmd & [arg-1 arg-2]] ele
+                      cmd-res (execute-cmd cmd acc arg-1 arg-2)]
+                  (trans cmd-res)))
+              "abcdefgh"
+              instructions)]
+    (apply str res)))
+
+;;
+;; I replaced my instructions with Bruce's, thinking I must have got one wrong. Result from running
+;; Bruce's instructions is:
+;; "dhcegafb"
+;; Result from mine is:
+;; "fdgeabhc"
+;;
+;; From Bruce's notes this is the right answer:
+;; "gbhafcde", but that's CRAP
+;; ---------------> "agcebfdh" GETS STAR, running his code
+;; So more than the instructions are wrong!
+;; Unsurprisingly my commands with Bruce's instructions produces another (wrong) result again:
+;; "bghcedfa"
+;; My function that was in error was -swap-letter, so have fixed that.
+;; I've checked order of instructions, that all are called, and swapped params around to see if the right
+;; answer can come out, all to no avail. Also checked that every instruction is in the file.
+;; Next I should clone his repo and compare all intermediate results, to see where they differ.
+;; Bruce 4th, 5th: (h c d a e b f g) [b c d a e h f g]
+;; Me 4th, 5th: hcdaebfg hcdaebfg
+;; So my parser is somehow ignoring instruction that swaps first and sixth letters, actually:
+;; "swap letter h with letter b"
+;; swap-letter is the one I got wrong, so maybe new untested version is doing nothing x-5 to test.
+;; Indeed problem is it needs to be a letter coming in, so using spec would have caught this error.
+;;
 (defn x []
   (let [
-        ;raw-input (slurp "./advent/twenty_one.txt")
+        raw-input (slurp "./advent/twenty_one.txt")
         ;raw-input steps
-        ;in (line-seq (BufferedReader. (StringReader. raw-input)))
-        instructions (retrieve-instructions steps)]
-    instructions))
+        in (line-seq (BufferedReader. (StringReader. raw-input)))
+        instructions (retrieve-instructions in)]
+    ;(pp/pprint instructions)
+    (run-instructions instructions)
+    ))
 
 (defn x-2 []
-  ((swap-letter-hof "o" "i") "holiday"))
+  (swap-letter "holiday" "o" "i"))
 
 (defn x-3 []
-  ((move 0 2) "abcd"))
+  (rotate-position "ecabd" "d"))
 
 ;swap position 4 with position 0 swaps the first and last letters, producing the input for the next step, ebcda.
 ;swap letter d with letter b swaps the positions of d and b: edcba.
@@ -133,4 +240,18 @@
 ; equal to that index, plus an additional time because the index was at least 4, for a total of 6 right rotations: decab.
 (defn x-4 []
   (let [input "abcde"
+        position-swapped (swap-position input 4 0)
+        letter-swapped (swap-letter position-swapped "d" "b")
+        reversed (reverse-span letter-swapped 0 4)
+        rotated (left-rotate reversed 1)
+        moved-1 (move rotated 1 4)
+        moved-2 (move moved-1 3 0)
+        rotated-1 (rotate-position moved-2 "b")
+        rotated-2 (rotate-position rotated-1 "d")
+        _ (println rotated-2)
         ]))
+
+;; Bruce 4th, 5th: (h c d a e b f g) [b c d a e h f g]
+;; "swap letter h with letter b"
+(defn x-5 []
+  (fixed-swap-letter "hcdaebfg" "h" "b"))
