375befcccb4e231aa6d50bb9c26d668e067819a9
Chris Murphy <chris@seasoft.com.au>
2017-12-04 13:11:01 +0800

    Transducer variety for day02

---
+++ src/advent_2017/day02_transducer.clj
@@ -0,0 +1,46 @@
+(ns advent-2017.day02-transducer
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [utils :as u]))
+
+(defn to-ints [xs]
+  (mapv #(Integer/parseInt %) xs))
+
+(defn get-input []
+  (->> (io/resource "2017/day02")
+       slurp
+       s/split-lines
+       (mapv (comp to-ints #(re-seq #"\d+" %)))))
+
+(defn row-checksum-1 [xs]
+  (->> xs
+       sort
+       ((juxt (partial apply max) (partial apply min)))
+       (apply -)
+       ))
+
+(def xform-row->evenly-divided (comp
+                                 (map (partial sort-by -))
+                                 (map (partial apply /))
+                                 (filter int?)))
+
+(defn row->evenly-divided-2 [xs]
+  (->> xs
+       (into [] xform-row->evenly-divided)
+       first))
+
+(defn row->evenly-divided-1 [xs]
+  (->> xs
+       (map (fn [xy]
+               (let [res (apply / (sort-by - xy))]
+                 res)))
+       (filter int?)
+       first))
+
+;; ans 280
+(defn x-2 []
+  (let [input (get-input)]
+    (->> input
+         (map #(u/combinations % 2))
+         (map row->evenly-divided-2)
+         (reduce +))))
